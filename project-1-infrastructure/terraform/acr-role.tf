8e62449419bb70d2af37bce01b977fb4e5eef27c
Madhu R <madhurohith29@gmail.com>
2026-08-30 17:50:04 +0530

    Add Azure login and ACR push to CI pipeline

--- project-1-infrastructure/terraform/acr-role.tf
+++ project-1-infrastructure/terraform/acr-role.tf
@@ -3,3 +3,9 @@ resource "azurerm_role_assignment" "acr_pull" {
   role_definition_name = "AcrPull"
   principal_id         = azurerm_linux_virtual_machine.app.identity[0].principal_id
 }
+
+resource "azurerm_role_assignment" "github_acr_push" {
+  scope                = azurerm_container_registry.main.id
+  role_definition_name = "AcrPush"
+  principal_id         = "57b4346b-a280-4f0b-8ed6-a44cbd7c18bb"
+}
